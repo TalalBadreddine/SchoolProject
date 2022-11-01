53ce74ecf5e5fdb64f6e93b1a2f73ba5bc71a054
talal <talalbadreddine@hotmail.com>
2022-11-01 22:46:36 +0200

    -Remove not used code in class_repo

--- repository/class_repo.go
+++ repository/class_repo.go
@@ -1,47 +1,11 @@
 package repository
 
 import (
-	"fmt"
 	"server/entity"
 	"server/filter"
 	"server/storage"
 	"strings"
 )
-
-func AddClass(class entity.Class) *entity.Class {
-	var db = storage.GetDBInstance()
-
-	err := db.Create(&class)
-
-	if err.Error != nil {
-		return nil
-	}
-
-	return &class
-}
-
-func GetClassById(classId int) entity.Class {
-	var db = storage.GetDBInstance()
-
-	var class entity.Class
-
-	db.First(&class, classId)
-
-	return class
-}
-
-func GetAllClasses() []*entity.Class {
-	var db = storage.GetDBInstance()
-	var classes []*entity.Class
-
-	db.Find(&classes)
-
-	return classes
-}
-
-func DisplayClass(class entity.Class) string {
-	return fmt.Sprintf("ID: %v ,Subject: %v , Class Code: %v \n", class.ID, class.Subject, class.Code)
-}
 
 func SearchClasses(filter filter.ClassFilter) []*entity.Class {
 	var db = storage.GetDBInstance()
