31d3af0723e33aadd6a1b212b15d30b7d2918b46
talal <talalbadreddine@hotmail.com>
2022-11-01 23:45:29 +0200

    -Remove not used filter file

--- repository/filter.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-type Filter struct {
-	Class    string `json:"class"`
-	Student  string `json:"student"`
-	Teacher  string `json:"teacher"`
-	Page     int    `json:"page"`
-	PageSize int    `json:"pageSize"`
-}
